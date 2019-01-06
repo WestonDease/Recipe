2fe34ea626ec6d3a73a487d789f63e2b96303fe7
weston <westondease@gmail.com>
2019-01-06 02:23:00 -0500

    Updates

---
+++ precache-manifest.b9809deb117fe87bf1f26cc25101adda.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd7ff0099639d2ccfc29",
+    "url": "/Recipe/static/js/runtime~main.cd7ff009.js"
+  },
+  {
+    "revision": "af4eba6f1f702c49ab81",
+    "url": "/Recipe/static/js/main.af4eba6f.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/Recipe/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "a34867598f58c50c738befc5ce937357",
+    "url": "/Recipe/index.html"
+  }
+];
